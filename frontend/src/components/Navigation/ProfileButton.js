140184dc3a2534364084810e4af6d0f175692b52
Roger Camps <rogercampsc@gmail.com>
2022-04-01 14:32:57 -0700

    setup modal login

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -31,7 +31,9 @@ function ProfileButton({ user }) {
   return (
     <>
       <button onClick={openMenu}>
-        <i className="fas fa-user-circle" />
+        <div id="profile-icon">
+            <i className="fas fa-user-circle" />
+        </div>
       </button>
       {showMenu && (
         <ul className="profile-dropdown">
