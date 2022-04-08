0b1beb5f5bfe6ea946bb0fbe6ae88e56f864b2ea
Roger Camps <rogercampsc@gmail.com>
2022-04-08 16:19:43 -0700

    second feature delete comment complete

--- frontend/src/components/Comment/CommentComponentIndex.js
+++ frontend/src/components/Comment/CommentComponentIndex.js
@@ -6,8 +6,9 @@ import './CommentComponent.css'
 
 const CommentComponentIndex = () => {
   const dispatch = useDispatch();
-  const { photoId } = useParams()
+  const { photoId, commentId } = useParams()
   const comments = useSelector(state => Object.values(state.comment));
+  const comment = useSelector(state => state.photo[commentId])
 
   const handleDelete = (photoId) => {
     dispatch(deleteComment(photoId));
@@ -17,15 +18,23 @@ const CommentComponentIndex = () => {
     dispatch(getAllComments());
   }, [dispatch])
 
+  // useEffect(() => {
+  //   dispatch(getAllComments(commentId))
+  // }, [dispatch, commentId]);
+
+  const sessionUser = useSelector(state => state.session.user)
+
   return (
     <main>
       <nav className="comment-index-nav">
         {comments.filter(comment => comment.imageId === +photoId).map(comment =>
           <div key={comment?.id} className="comment-index-div">
             {comment?.comment}
-            <button onClick={() => handleDelete(comment?.id)} className='comment-delete-btn'>
-              Delete
-            </button>
+            {sessionUser && sessionUser?.id === comment?.userId && (
+              <button onClick={() => handleDelete(comment?.id)} className='comment-delete-btn'>
+                Delete
+              </button>
+            )}
           </div>
         )}
       </nav>
@@ -34,3 +43,41 @@ const CommentComponentIndex = () => {
 }
 
 export default CommentComponentIndex;
+
+
+// import { useParams } from 'react-router-dom'
+// import { useEffect } from 'react';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { getAllComments, deleteComment } from '../../store/comment'
+// import './CommentComponent.css'
+
+// const CommentComponentIndex = () => {
+//   const dispatch = useDispatch();
+//   const { photoId } = useParams()
+//   const comments = useSelector(state => Object.values(state.comment));
+
+//   const handleDelete = (photoId) => {
+//     dispatch(deleteComment(photoId));
+//   };
+
+//   useEffect(() => {
+//     dispatch(getAllComments());
+//   }, [dispatch])
+
+//   return (
+//     <main>
+//       <nav className="comment-index-nav">
+//         {comments.filter(comment => comment.imageId === +photoId).map(comment =>
+//           <div key={comment?.id} className="comment-index-div">
+//             {comment?.comment}
+//             <button onClick={() => handleDelete(comment?.id)} className='comment-delete-btn'>
+//               Delete
+//             </button>
+//           </div>
+//         )}
+//       </nav>
+//     </main>
+//   )
+// }
+
+// export default CommentComponentIndex;
